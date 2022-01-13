2a6bda891d9932ccca6ba5122bfd4b6ea4cd9d56
waldofx <90760767+waldofx@users.noreply.github.com>
2022-01-13 18:59:15 +0700

    fix replies in comments

--- app/main.go
+++ app/main.go
@@ -114,13 +114,13 @@ func main() {
 	likeThreadsUsecase := _likeThreadsUsecase.NewLikeThreadsUsecase(likeThreadsRepo, timeoutContext)
 	likeThreadsCtrl := _likeThreadsController.NewLikeThreadsController(likeThreadsUsecase)
 
-	commentsRepo := _commentsRepo.NewCommentsRepository(db)
-	commentsUsecase := _commentsUsecase.NewCommentsUsecase(commentsRepo, timeoutContext)
-	commentsCtrl := _commentsController.NewCommentsController(commentsUsecase)
-
 	repliesRepo := _repliesRepo.NewRepliesRepository(db)
 	repliesUsecase := _repliesUsecase.NewRepliesUsecase(repliesRepo, timeoutContext)
 	repliesCtrl := _repliesController.NewRepliesController(repliesUsecase)
+
+	commentsRepo := _commentsRepo.NewCommentsRepository(db)
+	commentsUsecase := _commentsUsecase.NewCommentsUsecase(commentsRepo, timeoutContext, repliesRepo)
+	commentsCtrl := _commentsController.NewCommentsController(commentsUsecase)
 
 	likeCommentsRepo := _likeCommentsRepo.NewLikeCommentsRepository(db)
 	likeCommentsUsecase := _likeCommentsUsecase.NewLikeCommentsUsecase(likeCommentsRepo, timeoutContext)
--- businesses/comments/usecase_test.go
+++ businesses/comments/usecase_test.go
@@ -5,6 +5,8 @@ import (
 	"infion-BE/businesses"
 	"infion-BE/businesses/comments"
 	_commentsMock "infion-BE/businesses/comments/mocks"
+	"infion-BE/businesses/replies"
+	_repliesMock "infion-BE/businesses/replies/mocks"
 	"os"
 	"testing"
 	"time"
@@ -13,15 +15,27 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-
 var (
 	commentsRepository _commentsMock.Repository
 	commentsUsecase    comments.Usecase
 	commentsDomain     comments.Domain
+	repliesRepository	_repliesMock.Repository
+	repliesDomain		replies.Domain
 )
 
 func TestMain(m *testing.M) {
-	commentsUsecase = comments.NewCommentsUsecase(&commentsRepository, 2)
+	commentsUsecase = comments.NewCommentsUsecase(&commentsRepository, 2, &repliesRepository)
+	repliesDomain = replies.Domain{
+		ID:        1,
+		CommentID: 1,
+		UserID:    1,
+		User:      "user1",
+		UrlImg:    "placeholder.jpg",
+		Reply:     "comment test.",
+		LikeCount: 1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
 	commentsDomain = comments.Domain{
 		ID:				1,
 		ThreadID: 		1,
@@ -29,8 +43,9 @@ func TestMain(m *testing.M) {
 		User:			"user1",
 		UrlImg: 		"placeholder.jpg",
 		Comment: 		"comment test.",
+		Replies:		[]replies.Domain{repliesDomain},
 		LikeCount:		1,
-		ReplyCount:	1,
+		ReplyCount:		1,
 		CreatedAt: 		time.Now(),
 		UpdatedAt: 		time.Now(),
 	}
@@ -62,6 +77,7 @@ func TestStore(t *testing.T){
 func TestGetByID(t *testing.T){
 	t.Run("GetByID | Valid", func(t *testing.T) {
 		commentsRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(commentsDomain, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, nil).Once()
 
 		ctx := context.Background()
 		result, err := commentsUsecase.GetByID(ctx, commentsDomain.ID)
@@ -87,11 +103,23 @@ func TestGetByID(t *testing.T){
 		assert.NotNil(t, err)
 		assert.Equal(t, businesses.ErrInternalServer, err)
 	})
+
+	t.Run("GetByID | InValid Replies", func(t *testing.T) {
+		commentsRepository.On("GetByID", mock.Anything, mock.AnythingOfType("int")).Return(commentsDomain, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, businesses.ErrInternalServer).Once()
+
+		ctx := context.Background()
+		_, err := commentsUsecase.GetByID(ctx, commentsDomain.ID)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, businesses.ErrInternalServer, err)
+	})
 }
 
 func TestGetComments(t *testing.T){
 	t.Run("GetComments | Valid", func(t *testing.T) {
 		commentsRepository.On("GetComments", mock.Anything).Return([]comments.Domain{commentsDomain}, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, nil).Once()
 
 		ctx := context.Background()
 		result, err := commentsUsecase.GetComments(ctx)
@@ -99,7 +127,7 @@ func TestGetComments(t *testing.T){
 		assert.Nil(t, err)
 		assert.Equal(t, []comments.Domain{commentsDomain}, result)
 	})
-
+	
 	t.Run("GetComments | InValid", func(t *testing.T) {
 		commentsRepository.On("GetComments", mock.Anything).Return([]comments.Domain{commentsDomain}, businesses.ErrNotFound).Once()
 
@@ -109,11 +137,23 @@ func TestGetComments(t *testing.T){
 		assert.NotNil(t, err)
 		assert.Equal(t, businesses.ErrNotFound, err)
 	})
+
+	t.Run("GetComments | InValid Replies", func(t *testing.T) {
+		commentsRepository.On("GetComments", mock.Anything).Return([]comments.Domain{commentsDomain}, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, businesses.ErrInternalServer).Once()
+
+		ctx := context.Background()
+		_, err := commentsUsecase.GetComments(ctx)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, businesses.ErrInternalServer, err)
+	})
 }
 
 func TestGetCommentsByThreadId(t *testing.T){
 	t.Run("GetCommentsByThreadId | Valid", func(t *testing.T) {
 		commentsRepository.On("GetCommentsByThreadID", mock.Anything, mock.AnythingOfType("int")).Return([]comments.Domain{commentsDomain}, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, nil).Once()
 
 		ctx := context.Background()
 		result, err := commentsUsecase.GetCommentsByThreadID(ctx, commentsDomain.ThreadID)
@@ -124,12 +164,23 @@ func TestGetCommentsByThreadId(t *testing.T){
 
 	t.Run("GetCommentsByThreadId | InValid", func(t *testing.T) {
 		commentsRepository.On("GetCommentsByThreadID", mock.Anything, mock.AnythingOfType("int")).Return([]comments.Domain{commentsDomain}, businesses.ErrNotFound).Once()
-
+		
 		ctx := context.Background()
 		_, err := commentsUsecase.GetCommentsByThreadID(ctx, commentsDomain.ThreadID)
 
 		assert.NotNil(t, err)
 		assert.Equal(t, businesses.ErrNotFound, err)
+	})
+
+	t.Run("GetCommentsByThreadId | InValid Replies", func(t *testing.T) {
+		commentsRepository.On("GetCommentsByThreadID", mock.Anything, mock.AnythingOfType("int")).Return([]comments.Domain{commentsDomain}, nil).Once()
+		repliesRepository.On("GetRepliesByCommentID", mock.Anything, mock.AnythingOfType("int")).Return([]replies.Domain{repliesDomain}, businesses.ErrInternalServer).Once()
+
+		ctx := context.Background()
+		_, err := commentsUsecase.GetCommentsByThreadID(ctx, commentsDomain.ThreadID)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, businesses.ErrInternalServer, err)
 	})
 }
 
